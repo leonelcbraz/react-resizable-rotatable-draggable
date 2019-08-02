b5e4830c795c45af0ce0ca3919c33d5a417dc1d8
Leonel Costa Braz <leonelcbraz@gmail.com>
2019-08-02 19:28:17 -0300

    no message

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: 'babel-eslint',
-  extends: [ 'standard', 'standard-react' ],
-  plugins: [ 'react' ],
-  rules: {
-    'jsx-quotes': [ 'error', 'prefer-double' ]
-  }
-}
